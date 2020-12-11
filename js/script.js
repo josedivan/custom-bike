ceb90cd4dfa58b81eb8df6ddae7b78d874fe75ef
Edivan <edivanlima23@gmail.com>
2020-12-10 23:46:06 -0300

    ajustes finais

--- js/script.js
+++ js/script.js
@@ -13,3 +13,12 @@ if (window.SimpleSlide) {
 if (window.SimpleAnime) {
   new SimpleAnime();
 }
+
+if (window.SimpleForm) {
+new SimpleForm({
+  form: ".formphp", // seletor do formulário
+  button: "#enviar", // seletor do botão
+  erro: "<div id='form-erro'><h2>Erro no envio!</h2><p>Um erro ocorreu, tente para o email contato@bikcraft.com.</p></div>", // mensagem de erro
+  sucesso: "<div id='form-sucesso'><h2>Formulário enviado com sucesso</h2><p>Em breve eu entro em contato com você.</p></div>", // mensagem de sucesso
+});
+}
